5391ae4c2b252a1aa829639a0525178e65972b34
Nico <nicolamelito.11@gmail.com>
2022-10-18 23:11:42 +0200

    ancora test

--- assets/images/WelcomeImg.js
+++ assets/images/WelcomeImg.js
@@ -8,7 +8,7 @@ export default function ImgLogin() {
 
 return (
     <Image
-    style={{width: 350, height: 100, paddingTop: 150, marginTop: 250, marginBottom: 100}}
+    style={{width: 300, height: 100, paddingTop: 150, marginTop: 250, marginBottom: 100}}
         source={require('../images/logo-store.png')}
     />
 );
