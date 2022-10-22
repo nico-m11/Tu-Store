b9cb1eb4b05f716be81a70850b9fef4239fe259d
Nico <nicolamelito.11@gmail.com>
2022-10-22 19:34:26 +0200

    fetch buon fine, in dati si visualizzano in console
    rendere le card dinamiche con i dati che ci prendiamo

--- src/screens/bitcoin/Teso.js
+++ src/screens/bitcoin/Teso.js
@@ -27,15 +27,17 @@ export const TesoScreen = ({ navigation }) => {
   const Data = () => {
     fetch("https://be.control-room.app/api/salesChannels?page=1&limit=20", {
       method: "GET",
-      headers: { 
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlRVTzYiLCJpZCI6NiwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjY1MTQ4MDU5LCJleHAiOjE2Njc4MjY0NTl9.ake1PT2VGDret0lEFtXDTEA_HQR3CS5nVIFkeVF8XFI'    },
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlRVTzYiLCJpZCI6NiwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjY1MTQ4MDU5LCJleHAiOjE2Njc4MjY0NTl9.ake1PT2VGDret0lEFtXDTEA_HQR3CS5nVIFkeVF8XFI",
+      },
     })
       .then((res) => {
-        return res.json();       
+        return res.json();
       })
       .then((value) => {
-        var data = value
-        console.log(data)
+        var data = value.items;
+        //console.log(data)
         setListData(data);
       })
       .catch((err) => {
@@ -59,7 +61,11 @@ export const TesoScreen = ({ navigation }) => {
           flex: 1,
         }}
       >
-    
+        {listData.map((value) => {
+          var products = value.offers;
+            console.log(products)
+          //products.map((hello) => console.log(hello) )
+        })}
       </View>
       {/* <ExitModel
         onCancel={() => setExitModel(!exitModel)}
