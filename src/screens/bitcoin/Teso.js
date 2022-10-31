28dbbd998c2dd6781a2aaa4a811352d866b040f1
Nico <nicolamelito.11@gmail.com>
2022-10-31 17:28:12 +0100

    fetch data andato a buon fine
    adesso stampiamo anche tutta la lista, bisogna solo sistemare graficamente il tutto

--- src/screens/bitcoin/Teso.js
+++ src/screens/bitcoin/Teso.js
@@ -6,40 +6,44 @@ import {
   FlatList,
   TouchableOpacity,
   Image,
+  ScrollView,
   BackHandler,
 } from "react-native";
 import { Fonts } from "../../themes/fonts";
 import ConstantStyle from "../../themes/styles";
 import { Colors } from "../../themes/colors";
 import CustomHeader from "../components/CustomHeader";
-import MarketData from "../../data/MarketData";
 import { useTranslation } from "react-i18next";
 import ExitModel from "../components/ExitModel";
 import { Provider } from "react-redux";
 
 export const TesoScreen = ({ navigation }) => {
   const { t, i18n } = useTranslation();
-  const [listData, setListData] = useState([]);
+  const [listData, setListData] = useState([]); // salvo i dati del fetch
 
   useEffect(() => {
     Data();
   }, []);
 
+
+  {/** fetch data GET COMPARA TU*/}
   const Data = () => {
-    fetch("https://be.control-room.app/api/salesChannels?page=1&limit=10", {
-      method: "GET",
-      headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlRVTzYiLCJpZCI6NiwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjY1MTQ4MDU5LCJleHAiOjE2Njc4MjY0NTl9.ake1PT2VGDret0lEFtXDTEA_HQR3CS5nVIFkeVF8XFI",
-      },
-    })
+    fetch(
+      "https://be.control-room.app/api/salesChannels?page=1&limit=10&code=ComparaTu",
+      {
+        method: "GET",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlRVTzYiLCJpZCI6NiwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjY1MTQ4MDU5LCJleHAiOjE2Njc4MjY0NTl9.ake1PT2VGDret0lEFtXDTEA_HQR3CS5nVIFkeVF8XFI",
+        },
+      }
+    )
       .then((res) => {
         return res.json();
       })
       .then((value) => {
-        var data = value.items;
-        console.log(data)
-        setListData(data);
+        //console.log(value)
+        setListData(value.items); // salvo nel array i dati del fetch
       })
       .catch((err) => {
         console.log(err);
@@ -61,32 +65,28 @@ export const TesoScreen = ({ navigation }) => {
           borderTopRightRadius: 30,
           flex: 1,
         }}
-        >
-        {listData.map((value, index) => {
-          console.log(value)
-          
-    
-        
-
-          // if(value.code === 'CompraTu') {
-          //  console.log(value.offers)
-          // } else if (value.code === 'CrediTu') {
-
-          // } else if (value.code === ' AssicuraTu') {
-
-          // } else if(value.code === 'TuMarket') {
-
-          // } else if(value.code === 'Tutela') {
-
-          // } 
-  
-          // console.log(products[0].name)
-          // return(
-          //   <View key={index} style={{marginTop: 50}}>
-          //     <Text key= {products[0].id} >{products[0].name}</Text>
-          //   </View>
-          // );
-        })}
+      >
+        {/*** fetch title */}
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={{marginLeft: 30, marginTop: 10, marginBottom: 30}}>
+            {listData.map((element, index) => (
+              <View>
+                <Text key={index} style={{fontSize: 20}}>{element.code}</Text>
+              </View>
+            ))}
+          </View>
+          {/*** fetch data */}
+          <View>
+            {listData.map((element, index) =>
+              element.offers.map((el, i) => (
+                <View style={{ marginLeft: 30, marginBottom: 3}}>
+                  <Text key={i}>{el.name}</Text>
+                  <Text key={i}>{el.value}</Text>
+                </View>
+              ))
+            )}
+          </View>
+        </ScrollView>
       </View>
       {/* <ExitModel
         onCancel={() => setExitModel(!exitModel)}
