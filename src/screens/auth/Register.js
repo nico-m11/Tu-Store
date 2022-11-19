71633730653ef69fa322b5724a6b5a58b65b673e
Nico <nicolamelito.11@gmail.com>
2022-11-19 10:53:48 +0100

    check on android data piker parte 2

--- src/screens/auth/Register.js
+++ src/screens/auth/Register.js
@@ -71,26 +71,19 @@ export const RegisterScreen = ({ navigation }) => {
     }
   };
 
+  
+
   const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate;
+    setShow(false);
     setDate(currentDate);
   };
 
-
-  const showMode = (currentMode) => {
-    if (Platform.OS === 'android') {
-      setShow(false);
-      // for iOS, add a button that closes the picker
-    }
-    setMode(currentMode);
-  };
-
-
-  const showDatepicker = () => {
-    showMode('date');
-  };
 
   return (
     <SafeAreaView style={{ ...ConstantStyle.container }}>
