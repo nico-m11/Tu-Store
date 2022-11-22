1a8ce34a5238827b5c6dd4ee04f953140ca294ed
Nico <nicolamelito.11@gmail.com>
2022-11-22 21:22:19 +0100

    document Piker funzionante in pagine Teso (test)

--- src/screens/bitcoin/Teso.js
+++ src/screens/bitcoin/Teso.js
@@ -22,33 +22,27 @@ import ExitModel from "../components/ExitModel";
 import { Provider } from "react-redux";
 import { Formik } from 'formik';
 import * as yup from 'yup'
-import DocumentPicker , { types } from 'react-native-document-picker';    
-
+import * as DocumentPicker from 'expo-document-picker';
 
 export const TesoScreen = ({ navigation }) => {
   const { t, i18n } = useTranslation();
   const [image, setImage] = useState(null);
 
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
+  const pickDocument = async () => {
 
+    let result = await DocumentPicker.getDocumentAsync({});
+    
+    alert(result.uri);
+    
     console.log(result);
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
+    
     }
-  };
-
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <Button
+        title="Select Document"
+        onPress={pickDocument}
+        />
     </View>
   );
 
